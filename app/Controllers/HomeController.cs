ebc4b9f1aca2b2172a95671e04f31156f7b9916e
e.nasiri <e.nasiri@pki.co.ir>
2019-12-11 18:07:14 +0330

    Main

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using app.Models;
 using System.Net.Http;
+using MinMax;
 
 namespace app.Controllers
 {
@@ -21,13 +22,40 @@ namespace app.Controllers
 
         public async Task<IActionResult> Index()
         {
-
-            var url = "https://www.sahamyab.com/api/proxy/symbol/treeMap?v=0.1&type=volume&market=1,2,4&sector=&timeFrame=day&mini=false&date=1398/09/20&";
+            string todayDate = "1398/09/20";
+            var todayDirectoryName = todayDate.Replace("/", "_");
+            var url = $"https://www.sahamyab.com/api/proxy/symbol/treeMap?v=0.1&type=volume&market=1,2,4&sector=&timeFrame=day&mini=false&date={todayDate}&";
+            var stockwatch = "https://www.sahamyab.com/api/proxy/symbol/getSymbolExtData?v=0.1&code={0}&stockWatch=1&";
             var client = new HttpClient();
             var uri = new Uri(url);
-            string response = await client.GetStringAsync(uri);
+            string result = await client.GetStringAsync(uri);
+            //string result = System.IO.File.ReadAllText(@"D:\treeMap.json");
 
-            ViewBag.Data = response;
+            result = result.Replace("$color", "color");
+
+            var res = Newtonsoft.Json.JsonConvert.DeserializeObject<SYMain>(result);
+            List<orderdAllData> lstOrdred = new List<orderdAllData>();
+            if (!System.IO.Directory.Exists($"\\{todayDirectoryName}"))
+                System.IO.Directory.CreateDirectory($"\\{todayDirectoryName}");
+            foreach (var fItem in res.children)
+            {
+                foreach (var item in fItem.children)
+                { 
+                    var stockwatchFinalUrl = string.Format(stockwatch, item.name);
+                    string stockwatchResult = await client.GetStringAsync(stockwatchFinalUrl);
+                    System.IO.File.WriteAllText($"\\{todayDirectoryName}\\{item.name}", stockwatchResult);
+                    lstOrdred.Add(new orderdAllData()
+                    {
+                        name = item.name + " \t\t\t " + item.data.description,
+                        percent = item.data.color,
+                    });
+                }
+            }
+
+            var final = lstOrdred.OrderBy(n => n.percent);
+            ViewBag.Data = final;
+
+            System.IO.File.WriteAllText($"\\{todayDirectoryName}\\3m.log", result);
             return View();
         }
 
---
+++ app/SYMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMax
+{
+
+    public class orderdAllData
+    {
+        public string name { get; set; }
+        public double percent { get; set; }
+    }
+
+    public class SYMain
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<Children> children { get; set; }
+    }
+
+    public class Children
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<Children> children { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string description { get; set; }
+        public double price { get; set; }
+        public double color { get; set; }
+    }
+}
